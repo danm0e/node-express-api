a07117afc42a7c4d6d7ff25e6de511f1347049a6
Dan Moe <danmoe@gmail.com>
2025-11-20 17:28:40 +0000

    feat: add db connection scripts and pooling

---
+++ src/db/connection.ts
@@ -0,0 +1,17 @@
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
+import * as schema from './schema.ts'
+import { env, isProd } from '../../env.ts'
+import { remember } from '@epic-web/remember'
+import type { DrizzleConfig } from 'drizzle-orm'
+
+const createPool = () => {
+  const pool = new Pool({ connectionString: env.DATABASE_URL })
+  return pool
+}
+
+let client: any
+
+isProd() ? (client = createPool()) : (client = remember('dbPool', createPool()))
+
+export const db = drizzle(client, { schema })
